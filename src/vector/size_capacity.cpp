897cd505a8464f5c1eb519755f7917be52f91b85
Samuel Cohen <sam.cohen2001@gmail.com>
2023-08-09 10:24:27 +0200

    Exercise 2.1

--- src/vector/size_capacity.cpp
+++ src/vector/size_capacity.cpp
@@ -18,5 +18,16 @@ int main()
              << " capacity: " << vec.capacity() << endl;
     }
 
+    /*
+        The vector's size is the number of elements it contains that have been added into the vector. Capacity is the number of elements it is able
+        to contain before needing to reallocate memory for the vector.
+    */
+
+   /*
+        The capacity of the vector doubles each time the vector requires more contiguous space in memory. It does this in order to create a
+        balance between memory overhead with performance. We do not want to have too many memory allocations and we do not want to have a large
+        amount of unused allocated memory space. Doubling helps us find a good balance between these two.
+   */
+
     return 0;
 }
